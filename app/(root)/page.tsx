d8cc2173e001ac21b505a9265588cd4a9a070703
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2025-09-06 23:39:16 +0100

    feat: how it works home

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
 import { HeroBlock, Navbar } from "./components";
+import { HowItWorks } from "./components/HowItWorks";
 
 export default function Home() {
   return (
     <div className="min-h-screen w-full bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900">
       <Navbar />
       <HeroBlock />
+      <HowItWorks />
     </div>
   );
 }
